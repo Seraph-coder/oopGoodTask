977787ca8151d0f3ba55f6e65ab8df9671b35140
Marchenko Dmitriy <dimamarch2014@gmail.com>
2025-10-18 18:41:47 +0500

    Сделал 1 задание

--- src/main/java/ru/oop/task1/MainTask1.java
+++ src/main/java/ru/oop/task1/MainTask1.java
@@ -22,7 +22,9 @@ public class MainTask1 {
      * Переехать из текущего места в заданную точку
      */
     public void moveTo(Person person, Position destination) {
-        // TODO
+        Car myCar = new Car();
+        myCar.moveTo(person, destination);
+        person.walk(destination);
         assert person.getPosition() == destination;
     }
 }
